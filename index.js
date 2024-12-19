9bcb7b7d0a884a668e55ed888d6c65a3336d3d2d
ashrafulhossain1 <ahmeadashraful@gmail.com>
2024-12-19 16:21:54 +0600

    add cookie comment

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.use(cors())
-app.use(express.json())
+// app.use(cors({
+//     origin: ['http://localhost:5173'],
+//     credentials: true
+// }));
+// app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jkfsd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,9 +44,19 @@ async function run() {
 
         // auth related API
         app.post('/jwt', async (req, res) => {
-            const user = req.body;
-            const token = jwt.sign(user, 'secret', { expiresIn: '1h' })
-            res.send(token)
+            // const user = req.body;
+            // const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '1h' })
+            // res
+            //     .cookie('token', token, {
+            //         httpOnly: true,
+            //         secure: false, // if http (false) for development, if https (true) for production
+
+            //     })
+            //     .send({ success: true })
+
+
+
+
         })
 
 
@@ -81,8 +99,11 @@ async function run() {
         // with take job details by aggregate 82 no line
         app.get('/job-applications', async (req, res) => {
             const email = req.query.email;
-
             const query = { applicantEmail: email }
+
+            // console.log(' cuk cuk cookies', req.cookies)
+
+
             const result = await jobApplicationCollection.find(query).toArray()
 
             // Fokira way to aggregate data
